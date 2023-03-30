532b4f6ab096d0590bad99ca607db580b945dbcb
ChukwudiNebo <chukwudineboch@gmail.com>
2023-03-30 06:52:16 -0700

    more styling done

--- src/Components/pages/blog post/BlogPost.js
+++ src/Components/pages/blog post/BlogPost.js
@@ -1,21 +1,49 @@
-import React from 'react'
+import React from "react";
+import "./blogpost.css";
+import BlogAuthor from "../../molecules/blog author and date/BlogAuthor";
+import Text from "../../atoms/text/Text";
 
-const BlogPost = ({ author, blogDate }) => {
+const BlogPost = () => {
   return (
     <>
-      <div>
-        <div>
-            <div>
-                <Te
-            </div>
-            <div></div>
+      <div className="blogPost">
+        <div className="blogPost__header">
+          <div className="blogPost__heading">
+            <Text
+              text="15 Disadvantages Of Freedom And How You Can
+Workaround It."
+            />
+          </div>
+          <div className="blogPost__author">
+            <BlogAuthor author="@samurai2099" blogDate="27 may 2022" />
+          </div>
         </div>
-        <div>
-
+        <div className="blogPost__paragraph">
+          <div>
+            <Text
+              text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, enim harum. Esse repellendus quae explicabo vel voluptatibus et similique inventore reprehenderit cupiditate, ipsum nesciunt, deleniti fuga qui quos. At, eum.
+            Omnis molestias distinctio, ea facere labore voluptatibus pariatur odio architecto ipsa delectus ex aperiam inventore laborum nulla quasi aliquam, suscipit assumenda? Repellat autem similique modi quae rerum? In, aut obcaecati.
+            Dolores dolor ab impedit, odio perspiciatis, optio, natus eaque esse suscipit ratione exercitationem enim. Tempora ipsa voluptatum dolore architecto quas. Ex suscipit vitae facilis odio enim nesciunt voluptatibus at atque."
+            />
+          </div>
+          <div className="blogPost__paragraph-1">
+            <Text
+              text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, enim harum. Esse repellendus quae explicabo vel voluptatibus et similique inventore reprehenderit cupiditate, ipsum nesciunt, deleniti fuga qui quos. At, eum.
+            Omnis molestias distinctio, ea facere labore voluptatibus pariatur odio architecto ipsa delectus ex aperiam inventore laborum nulla quasi aliquam, suscipit assumenda? Repellat autem similique modi quae rerum? In, aut obcaecati.
+            Dolores dolor ab impedit, odio perspiciatis, optio, natus eaque esse suscipit ratione exercitationem enim. Tempora ipsa voluptatum dolore architecto quas. Ex suscipit vitae facilis odio enim nesciunt voluptatibus at atque."
+            />
+          </div>
+          <div>
+            <Text
+              text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, enim harum. Esse repellendus quae explicabo vel voluptatibus et similique inventore reprehenderit cupiditate, ipsum nesciunt, deleniti fuga qui quos. At, eum.
+            Omnis molestias distinctio, ea facere labore voluptatibus pariatur odio architecto ipsa delectus ex aperiam inventore laborum nulla quasi aliquam, suscipit assumenda? Repellat autem similique modi quae rerum? In, aut obcaecati.
+            Dolores dolor ab impedit, odio perspiciatis, optio, natus eaque esse suscipit ratione exercitationem enim. Tempora ipsa voluptatum dolore architecto quas. Ex suscipit vitae facilis odio enim nesciunt voluptatibus at atque."
+            />
+          </div>
         </div>
       </div>
     </>
   );
 };
 
-export default BlogPost
+export default BlogPost;
